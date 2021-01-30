3a1e07da0b0b18bd15c4951af1cab4ad46c5dc00
Artur Kashperskiy <art@network.ru>
2021-01-29 16:43:39 -0800

    improved logging

--- bbbot/logging.py
+++ bbbot/logging.py
@@ -1,6 +1,8 @@
 import logging
 
-logging.basicConfig(level=logging.INFO)
-
+logging.basicConfig(
+         format='%(asctime)s | %(levelname)-8s | %(name)s | %(message)s',
+         level=logging.INFO,
+         datefmt='%Y-%m-%d %H:%M:%S')
 def logger_factory(name):
     return logging.getLogger(name)
